fd2f92325b4797291f64fb4dfcefe8a2a952c8cf
Amirhosein Arjmand <arjmandamirhosein79@gmail.com>
2023-03-05 02:22:49 +0330

    add styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "bootstrap/dist/css/bootstrap.rtl.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "@/styles/globals.css";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap.bundle.js");
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
